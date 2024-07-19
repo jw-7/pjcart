2f6372c80d6e9f5ad62401574078a06dcf0a9874
jw-7 <finreslee@gmail.com>
2024-07-20 04:40:26 +0900

    Initial commit

---
+++ src/main/java/com/pjcart/shop/PjcartApplication.java
@@ -0,0 +1,13 @@
+package com.pjcart.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjcartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjcartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjcart/shop/PjcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjcart.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjcartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
